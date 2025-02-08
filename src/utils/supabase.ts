1468526b7e8fc92f08ec495cd4e345cc7d45562b
Jeova Junior <jrjeova@hotmail.com>
2025-02-08 09:55:30 -0300

    Refactor Supabase client configuration and update package dependencies

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,16 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import "react-native-url-polyfill/auto";
 
 const SUPABASE_URL = "https://eciwmuwdqlzrryamjzjl.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjaXdtdXdkcWx6cnJ5YW1qempsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc0OTgwNzAsImV4cCI6MjA1MzA3NDA3MH0.YjfsJpPxv9ylyS_uFwrCybTY_a5bmcQ6DBzVqbyhS4Y";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
